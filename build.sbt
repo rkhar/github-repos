8a2a0d17a86db47da983691df6de19eed35aa8b2
rkhar <rassul.khar@gmail.com>
2022-08-01 23:54:59 +0600

    simple http server

--- build.sbt
+++ build.sbt
@@ -1,8 +1,19 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.1.3"
+ThisBuild / scalaVersion := "2.13.8"
 
 lazy val root = (project in file("."))
   .settings(
     name := "github-repos"
   )
+
+val akkaVersion = "2.6.19"
+val akkaHttpVersion = "10.2.9"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+
+  "org.scalameta" %% "scalafmt-dynamic" % "3.5.8"
+)
---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,34 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, path}
+import akka.http.scaladsl.server.Directives._
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
+object Boot {
+
+  def main(args: Array[String]): Unit = {
+
+    implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "my-system")
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      }
+
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done  }
+
+  }
+}
